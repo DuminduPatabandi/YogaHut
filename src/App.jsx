ad4857bc62d9fc7416fd3f12276f0e8126bec413
DuminduPatabandi <dppatabandi@gmail.com>
2023-12-28 11:18:55 +0530

    Google Analytics Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+// import { useEffect } from 'react'
+// import myGa from './myGa'
 import Home from "./components/Home"
 import Card from "./components/Card"
 
 function App() {
+
+  // useEffect(() => {
+  //   myGa()
+  // },[])
+
   return (
     <>
       <Home />
